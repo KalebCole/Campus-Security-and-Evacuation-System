5951a1148b82990b10ecf1c1b78cab46a7fa6c17
Kaleb Cole <93733756+KalebCole@users.noreply.github.com>
2025-01-22 09:16:52 -0500

    chore: update startup script to use relative paths for server and virtual environment activation

--- startup.ps1
+++ startup.ps1
@@ -1,7 +1,11 @@
-Set-Location -Path "server"
+# Get the directory of the script
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
-# activate the virtual environment
-& "venv\Scripts\activate"
+# Set the location to the server directory relative to the script's location
+Set-Location -Path "$scriptDir\server"
 
-# start the server
-& "python.exe" ".\app.py"
+# Activate the virtual environment
+& "$scriptDir\server\venv\Scripts\activate"
+
+# Start the server
+& "python.exe" "$scriptDir\server\app.py"
