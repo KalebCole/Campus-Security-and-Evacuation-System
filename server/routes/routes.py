30c7fa55081623bd599dc73ccda1a0a7b8a3b9c6
Kaleb Cole <93733756+KalebCole@users.noreply.github.com>
2025-01-28 10:00:19 -0500

    chore: commented out supabase stuff

--- server/routes/routes.py
+++ server/routes/routes.py
@@ -152,80 +152,76 @@ def send_embedding_to_server(embedding):
 # - Update a user by ID
 # - Delete a user by ID
 
-@app.route('/upload-image', methods=['POST'])
-def upload_image():
+# @routes_bp.route('/upload-image', methods=['POST'])
+# def upload_image():
 
-    data = request.get_json()
-    # validate the request
-    if not data:
-        return jsonify({"error": "No data provided"}), 400
+#     data = request.get_json()
+#     # validate the request
+#     if not data:
+#         return jsonify({"error": "No data provided"}), 400
 
-    # validate the image data
-    if 'base64_image' not in data:
-        return jsonify({"error": "No image provided"}), 400
+#     # validate the image data
+#     if 'base64_image' not in data:
+#         return jsonify({"error": "No image provided"}), 400
 
-    # Extract the image data from the POST request
-    image_data = data.get('base64_image')
-    if image_data:
-        # Decode the base64-encoded image
-        image_bytes = base64.b64decode(image_data)
+#     # Extract the image data from the POST request
+#     image_data = data.get('base64_image')
+#     if image_data:
+#         # Decode the base64-encoded image
+#         image_bytes = base64.b64decode(image_data)
 
-        # Generate a unique file name or use a provided one
-        unique_filename = str(uuid.uuid4()) + '.jpg'
-        path_on_supastorage = f'{user_entries_storage_path}/{unique_filename}'
+#         # Generate a unique file name or use a provided one
+#         unique_filename = str(uuid.uuid4()) + '.jpg'
+#         path_on_supastorage = f'{user_entries_storage_path}/{unique_filename}'
 
-        # Upload the image bytes to Supabase storage
-        response = storage_bucket.upload(
-            path=path_on_supastorage, file=image_bytes, file_options={"content-type": "image/jpeg"})
+#         # Upload the image bytes to Supabase storage
+#         response = storage_bucket.upload(
+#             path=path_on_supastorage, file=image_bytes, file_options={"content-type": "image/jpeg"})
 
-        # Check if the image was uploaded successfully
-        if response.status_code != 200:
-            return jsonify({"error": "Failed to upload image"}), 500
+#         # Check if the image was uploaded successfully
+#         if response.status_code != 200:
+#             return jsonify({"error": "Failed to upload image"}), 500
 
-        # fetch from the storage
-        image_url = storage_bucket.get_public_url(path_on_supastorage)
-        print(image_url)
+#         # fetch from the storage
+#         image_url = storage_bucket.get_public_url(path_on_supastorage)
+#         print(image_url)
 
-        # validate the response
-        if not image_url:
-            return jsonify({"error": "Failed to get image url"}), 500
+#         # validate the response
+#         if not image_url:
+#             return jsonify({"error": "Failed to get image url"}), 500
 
-        # insert the user into the table
-        user = {"photo_url": image_url}
-        response = user_table.insert(user).execute()
-        print(response)
+#         # insert the user into the table
+#         user = {"photo_url": image_url}
+#         response = user_table.insert(user).execute()
+#         print(response)
 
-        # validate the response
-        if not response:
-            return jsonify({"error": "Failed to insert user"}), 500
+#         # validate the response
+#         if not response:
+#             return jsonify({"error": "Failed to insert user"}), 500
 
-        return jsonify({"message": "Image received successfully"}), 200
-    else:
-        return jsonify({"error": "No image provided"}), 400
-
-
-@app.route('/get-users', methods=['GET'])
-def get_users():
-    # Query the 'users' table in Supabase
-    response = user_table.select('*').execute()
-
-    # Print the response for debugging
-    print("in get users")
-    print(response)
-    # response data
-    print("response data: " + str(response.data))
-
-    print("json response data" + str(jsonify(response.data)))
-
-    # Return the data portion of the response
-    if response.data:
-        return jsonify(response.data), 200
-    else:
-        return jsonify({"error": "No data found"}), 404
+#         return jsonify({"message": "Image received successfully"}), 200
+#     else:
+#         return jsonify({"error": "No image provided"}), 400
 
 
+# @routes_bp.route('/get-users', methods=['GET'])
+# def get_users():
+#     # Query the 'users' table in Supabase
+#     response = user_table.select('*').execute()
 
+#     # Print the response for debugging
+#     print("in get users")
+#     print(response)
+#     # response data
+#     print("response data: " + str(response.data))
 
+#     print("json response data" + str(jsonify(response.data)))
+
+#     # Return the data portion of the response
+#     if response.data:
+#         return jsonify(response.data), 200
+#     else:
+#         return jsonify({"error": "No data found"}), 404
 
 
 # VALIDATION LOGIC
