cc82eca4be95b0849df0b9a5dfc2ab7d293f3039
Kaleb Cole <93733756+KalebCole@users.noreply.github.com>
2025-01-29 09:40:09 -0500

    refactor: changed  the startup script to move back to the root after the command (even if error)

--- startup.ps1
+++ startup.ps1
@@ -1,11 +1,21 @@
 # Get the directory of the script
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
-# Set the location to the server directory relative to the script's location
-Set-Location -Path "$scriptDir\server"
+# Save the current location
+$originalLocation = Get-Location
 
-# Activate the virtual environment
-& "$scriptDir\server\venv\Scripts\activate"
+try {
+    # Set the location to the server directory relative to the script's location
+    Set-Location -Path "$scriptDir\server"
 
-# Start the server
-& "python.exe" "$scriptDir\server\app.py"
+    # Activate the virtual environment
+    & "$scriptDir\server\venv\Scripts\activate"
+
+    # Start the server
+    & "python.exe" "$scriptDir\server\app.py"
+} catch {
+    Write-Error $_
+} finally {
+    # Return to the original location
+    Set-Location -Path $originalLocation
+}
